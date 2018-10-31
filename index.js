b3ec78d9f896f10236f78c8b0fec3023f8e6c307
daniel vicario <danigv.email@gmail.com>
2018-10-31 12:11:17 +0100

    understanding the button handlers with on and off

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
-function add(first, second, callback) {
-    var result = first + second;
-    callback(result);
-}
+// function add(first, second, callback) {
+//     var result = first + second;
+//     callback(result);
+// }
 
-add(6, 4, function (num) {
-    for (var i = 0; i < num; i++) {
-        console.log("We are anonymous!");
+// add(6, 4, function (num) {
+//     for (var i = 0; i < num; i++) {
+//         console.log("We are anonymous!");
+//     }
+// });
+
+
+$(document).ready(function () {
+    // Define variable in anonymous function
+    var z = 1;
+
+    function clickRecio(e) {
+        switch ($(e.target).attr("class")) {
+            case "b1":
+                console.log("Has clickado el botón 1::::: " + z)
+                break;
+            case "b2":
+                console.log("Destrucción del click handler. desasignación de memoria")
+                $('button').off("click");
+                break;
+        }
+
+
     }
+
+    $('button').on('click', clickRecio);
 });
